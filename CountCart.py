19a73da0bc4d668cba981bd6f67a80fe4cb45eae
Pierre Marsaa <51882862+AphroMad@users.noreply.github.com>
2021-03-31 18:12:35 +0200

    Update CountCart.py

--- CountCart.py
+++ CountCart.py
@@ -5,62 +5,60 @@ Created on Wed May 13 10:20:38 2020
 @author: pierr
 """
 
-""" Compteur de cartes blackjack"""
-
-""" Coup a jouer
+""" Probability
 https://fr.blackjack.org/wp-content/uploads/2017/09/basic-blackjack-strategy-522x1030.png
 """
 
-""" Système de comptage 
+""" Learn how to count 
 https://www.countingedge.com/fr/comptage-des-cartes/hi-opt-ii/
 """
 
 
-
+# function that print a list with all the probability in it 
 def print_line(tabl) : 
-    
     for i in range(len(tabl)) : 
         print(tabl[i])
 
 
-#sabot = int(input("Nombre de paquets de cartes : \t"))
+#sabot = int(input("Nombre de paquets de cartes : \t")) # if you want to enter the value of sabot each time 
 sabot = 8 
 
-cartes_couleur = ["coeur","carreau","pique","trèfle"] # la liste des symboles 
-cartes_valeur = ["1","2","3","4","5","6","7","8","9","10","11","12","13"] # la liste des valerurs 
-nbr_mm_carte = 4 * sabot # le nombre de cartes à mêmes valeurs totale
-nbr_tot = 52 * sabot # le nombre total de cartes dans le sabot 
+cartes_couleur = ["heart","diamonds","spades","clubs"] # list of color 
+cartes_valeur = ["1","2","3","4","5","6","7","8","9","10","11","12","13"] # list of card_value 
+nbr_mm_carte = 4 * sabot # number of each card (each king, each queen, etc...)
+nbr_tot = 52 * sabot # total number of cards
 
-proba_carte_depart = round(nbr_mm_carte / nbr_tot *100,2) # calcul de la proba de départ que chaque carte tombe
+proba_carte_depart = round(nbr_mm_carte / nbr_tot *100,2) # Calculate first probability 
 
-nombre_carte = []
-tot_now = nbr_tot 
+nombre_carte = [] # a list with all the card that'll be available 
+tot_now = nbr_tot # number total of cards 
 
-
-for i in range(len(cartes_valeur)) : 
-    nombre_carte.append([cartes_valeur[i],nbr_mm_carte,proba_carte_depart])
+for i in range(len(cartes_valeur)) : # we go through each cards available 
+    nombre_carte.append([cartes_valeur[i],nbr_mm_carte,proba_carte_depart]) # we add in a list the card, the number of the same, and the probability that it can be played 
 
     
-print_line(nombre_carte)
-
+print_line(nombre_carte) # print all the probability 
 
 while True : 
     
+    # which card i played ? 
     carte_now = int(input("Rentrer une carte : \n(V = 11, Q = 12, R = 13) \nEnter pour quitter \t")) # on demande à l'utilisateur de rentrer une carte qui est tombée
 
     for i in range(len(nombre_carte)) : 
         
-        if carte_now == i+1 : # on cherche l'emplacement de la carte choisie
+        if carte_now == i+1 : # we search the card played 
             
-            nombre_carte[i][1] = nombre_carte[i][1] - 1 # on réduis le nombre de dispo cette carte de 1      
-            tot_now = tot_now - 1 # on dis qu'on enlève une carte 
-            #nombre_carte[i][2] = round(nombre_carte[i][1] / tot_now*100,2) # on recalcule la proba 
+            nombre_carte[i][1] = nombre_carte[i][1] - 1 # reduce the number of this card available     
+            tot_now = tot_now - 1 # reduce the number of total cards 
+
             
     for i in range(len(nombre_carte)) : 
        
-        nombre_carte[i][2] = round(nombre_carte[i][1] / tot_now*100,2) # on recalcule les proba 
+        nombre_carte[i][2] = round(noclubsmbre_carte[i][1] / tot_now*100,2) # calculate the probability 
             
-    print_line(nombre_carte) # on affiche les nouvelles proba
+    print_line(nombre_carte) # print the new proba 
+    
+    # do it again ! 
                 
 
 
